4f9964f5c702099b861bb4696c9c61dcfe11acad
yooseungjun <hongildong6987@gmail.com>
2024-05-06 14:08:28 +0900

    chore : 바이트 읽기의 신

--- filesystems-checker/main.c
+++ filesystems-checker/main.c
@@ -29,24 +29,60 @@ int main(int argc, char* argv[])
     }
 
     // mapping fs.img to my address space. Starting from superblock
-    block_t* bp;
-    bp = mmap(NULL, sizeof(block_t) * fs_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    block_t* bp = mmap(0, BSIZE * fs_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (bp == MAP_FAILED) {
         goto error;
     }
-    bp++; // advance to superblock
 
+    
     // 된다. 블록 진행시키면서 그냥 정보 뽑으면됨.
     struct superblock* sb;
-    sb = (void*) bp;
-    printf("mmaped info size : %d, nblocks : %d, logstart: %d\n",sb->size, sb->nblocks, sb->logstart);
+    sb = (struct superblock*) (bp + 1); // advance to sector1 to get superblock information
+    printf("mmaped at %p@@ \ninfo size : %d, nblocks : %d, logstart: %d, inode start sector : %d\n", bp,fs_size, sb->nblocks, sb->logstart, sb->inodestart);
 
+    uint log_count = sb->nlog;
+    char* log_p = (char*) (bp + sb->logstart);
 
+    uint inodes_count = sb->ninodes;
+    // 이게 맞다..
+    struct dinode* inode_p = (void*) (bp + sb->inodestart); // fucking segfault 아마 어제는 0참조한듯
 
+    inode_p++; // to inode number 1, inode number 0, 블록시작자리에는 아무것도 없어서 어제 0 참조함
+
+    short a = inode_p->size;
+    uint* addrs = inode_p->addrs; // contains inode's data block's file block number
+    // if this is file, contains file byte
+    // if this is directory, contains directory entry data
+        // if dirent, use struct dirent
+    // currently this is root directory
+
+    // root inode number is 1, thus starting at offset 0x0 0004040 , 0x 4000 에는 inode 0번 자리인데 그런건 없음
+    printf("inode block starting at %p, root inode with number 1 is starting at %p\n", bp + sb->inodestart + 1, inode_p);
+    printf("inode struct size : %lu root folder size : %d\n", sizeof(struct dinode), a);
+    // root folder size should be 512
+    
+
+    uint data_block_count = sb->nblocks;
+    char* bmap_p = (char*) (bp + sb->bmapstart);
+    
+
+    // for (int i = 0; i < NDIRECT + 1; ++i) {
+    //     uint data_block_address = (uint) bp + root_inode->addrs[i];
+    //     if (data_block_address == 0) {
+    //         break;
+    //     }
+
+    //     char* dbp = (char*) data_block_address;
+    //     for (int j = 0; j < BSIZE; ++j) {
+    //         printf("%c", dbp[j]);
+    //     }
+    // }
+    
     
 error:
     perror("error flag setted ");
     if (fd != -1){
         close(fd);
     }
+    munmap((void*) bp, sizeof(block_t) * fs_size);
 }
--- filesystems-checker/types.h
+++ filesystems-checker/types.h
@@ -42,7 +42,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEV only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+1];   // Data block addresses, file block number, not specific disk location
 };
 
 // Inodes per block.
